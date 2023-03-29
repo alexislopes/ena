f50b6175cf2eefdcb9f7c9a9e492da392dbc39f9
Alexis Lopes <alexisdrops@gmail.com>
2023-03-29 20:22:10 -0300

    add transactions by resources computed 📩

--- store/resourcesStore.js
+++ store/resourcesStore.js
@@ -1,7 +1,15 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { useTransactionsStore } from "./transactionsStore";
 
 export const useResourcesStore = defineStore('resources', () => {
   const resources = ref([])
+  const transactionsStore = useTransactionsStore()
 
-  return { resources }
+  const transactionsByResources = computed(() => {
+    return resources.value.map(resource => Object.assign(resource, {
+      transactions: transactionsStore.lastTransactions.filter(transaction => transaction.Conta.toLowerCase().includes(resource.agregador))
+    }))
+  })
+
+  return { resources, transactionsByResources }
 });
