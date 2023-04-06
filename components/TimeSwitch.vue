8b9bbbf30c550f34c68e9c105e8ecc783c2aa777
Alexis Lopes <alexisdrops@gmail.com>
2023-04-06 00:07:40 -0300

    add week count to TimeSwitch component ⏳

--- components/TimeSwitch.vue
+++ components/TimeSwitch.vue
@@ -13,6 +13,12 @@ const month = computed(() => {
   return new Date(store.timestamp).toLocaleDateString('pt-BR', { month: 'long', year: 'numeric', day: 'numeric' }).split('de')[1]
 })
 
+const weekCount = computed(() => {
+  const ano = new Date(store.timestamp).getFullYear()
+  const mes = new Date(store.timestamp).getMonth()
+  return Math.ceil((new Date(ano, mes + 1, 0).getDate() + new Date(ano, mes, 1).getDay()) / 7)
+})
+
 const time = ref({
   weekly: 604800 * 1000,
   monthly: 2629743 * 1000,
@@ -24,9 +30,15 @@ const time = ref({
 <template>
   <div class="flex gap-2">
     <p @click="store.timestamp = store.timestamp - time[store.type]">&lt;</p>
-    <span @click="store.setType('weekly')">weekly {{ weekOfTimestamp(store.timestamp) }}</span>
-    <span @click="store.setType('monthly')">monthly {{ month }}</span>
-    <span @click="store.setType('yearly')">yearly {{ year }}</span>
+                  <div>
+                    <span>{{ month }} {{ year }}</span>
+                    <div class="flex gap-1">
+                      <span v-for="i in weekCount">S{{ i }}</span>
+                    </div>
+                  </div>
+                  <!-- <span @click="store.setType('weekly')">weekly {{ weekOfTimestamp(store.timestamp) }}</span>
+                  <span @click="store.setType('monthly')">monthly {{ month }}</span>
+                  <span @click="store.setType('yearly')">yearly {{ year }}</span> -->
     <p @click="store.timestamp = store.timestamp + time[store.type]">&gt;</p>
   </div>
 </template>
